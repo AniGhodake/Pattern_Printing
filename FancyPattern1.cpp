18fc095e9d5219567e6c5524f3ac34eaf57922e5
AniketGhodake <ghodakeaniket27@gmail.com>
2023-10-10 23:56:59 +0530

    10 Oct 2023

---
+++ FancyPattern1.cpp
@@ -0,0 +1,13 @@
+	// for(int row=0; row<n; row=row+1) {
+	// 	//innner loop
+	// 	for(int col=0; col<2*row+1; col=col+1) {
+	// 		if(col%2 ==  1) {
+	// 			//odd number                                    // FANCY PATTERN
+	// 			cout << "*";
+	// 		}
+	// 		else {
+	// 			cout << row+1;
+ 	// 		}
+	// 	}
+	// 	cout << endl;
+	// }
