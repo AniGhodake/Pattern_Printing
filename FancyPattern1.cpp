9e37ac46eaeda3cdc82e4895c9c0ab9357909545
AniketGhodake <ghodakeaniket27@gmail.com>
2023-10-13 22:30:44 +0530

    13 Oct 2023

--- FancyPattern1.cpp
+++ FancyPattern1.cpp
@@ -1,13 +1,21 @@
-	// for(int row=0; row<n; row=row+1) {
-	// 	//innner loop
-	// 	for(int col=0; col<2*row+1; col=col+1) {
-	// 		if(col%2 ==  1) {
-	// 			//odd number                                    // FANCY PATTERN
-	// 			cout << "*";
-	// 		}
-	// 		else {
-	// 			cout << row+1;
- 	// 		}
-	// 	}
-	// 	cout << endl;
-	// }
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+	for(int row=0; row<n; row=row+1) {
+		//innner loop
+		for(int col=0; col<2*row+1; col=col+1) {
+			if(col%2 ==  1) {
+				//odd number                                    // FANCY PATTERN
+				cout << "*";
+			}
+			else {
+				cout << row+1;
+ 			}
+		}
+		cout << endl;
+	}
+}
--- inveredHollowHalfPyramid.cpp
+++ inveredHollowHalfPyramid.cpp
@@ -1,3 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
 	for(int row=0; row<n; row=row+1) {
 		for(int col=0;col<n-row; col=col+1) {
 			if(row ==0 || row == n-1) {
@@ -15,3 +22,4 @@
 		}
 		cout << endl;
 	}
+}
