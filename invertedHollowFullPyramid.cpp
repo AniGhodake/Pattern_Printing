d9e137b7f11b067092e31b7e15c2b7ee02c8b973
AniketGhodake <ghodakeaniket27@gmail.com>
2023-10-14 22:29:30 +0530

    14 Oct 2023

--- invertedHollowFullPyramid.cpp
+++ invertedHollowFullPyramid.cpp
@@ -1,3 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
 for(int row=0; row<n; row=row+1) {
 		//spaces
 		for(int col=0; col<row; col=col+1) {
@@ -18,3 +25,4 @@ for(int row=0; row<n; row=row+1) {
 		}
 		cout << endl;
 	}
+}
