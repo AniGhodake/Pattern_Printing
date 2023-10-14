fbe11650d6655505149fe9f68a265efd3378d0e5
AniketGhodake <ghodakeaniket27@gmail.com>
2023-10-14 22:28:10 +0530

    14 Oct 2023

--- hollowFollPyramid.cpp
+++ hollowFollPyramid.cpp
@@ -1,4 +1,11 @@
-for(int row=0; row<n; row=row+1) {
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+    for(int row=0; row<n; row=row+1) {
 		//space
 		for(int col=0; col<n-row-1; col=col+1) {
 			cout <<" ";
@@ -18,3 +25,4 @@ for(int row=0; row<n; row=row+1) {
 		}
 		cout << endl;
 	}
+}
