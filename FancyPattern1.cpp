b0f2cd78651cf598ad6a7ebcc840cc4687456cf6
Aniket Sunil Ghodake <74956511+AniGhodake@users.noreply.github.com>
2023-10-23 23:37:14 +0530

    FancyPattern1.cpp

--- FancyPattern1.cpp
+++ FancyPattern1.cpp
@@ -1,15 +1,13 @@
 #include<iostream>
 using namespace std;
-
 int main() {
-
 	int n;
 	cin >> n;
 	for(int row=0; row<n; row=row+1) {
 		//innner loop
 		for(int col=0; col<2*row+1; col=col+1) {
 			if(col%2 ==  1) {
-				//odd number                                    // FANCY PATTERN
+				//odd number                          // FANCY PATTERN
 				cout << "*";
 			}
 			else {
