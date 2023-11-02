0e9cac826ead456e2c1651aff0e26974f0aba316
Aniket Sunil Ghodake <74956511+AniGhodake@users.noreply.github.com>
2023-11-02 22:51:57 +0530

    Update invertedFullPyramid.cpp

--- invertedFullPyramid.cpp
+++ invertedFullPyramid.cpp
@@ -5,7 +5,7 @@ int main() {
 
 	int n;
 	cin >> n;
-for(int row=0; row<n; row=row+1) {
+	 for(int row=0; row<n; row=row+1) {
 		//spaces
 		for(int col=0; col<row; col=col+1) {
 			cout << " ";
@@ -17,4 +17,16 @@ for(int row=0; row<n; row=row+1) {
 		}
 		cout << endl;
 	}
+ //        for(int row=0; row<n; row=row+1) {
+	// 	//spaces
+	// 	for(int col=0; col<row; col=col+1) {
+	// 		cout << " ";
+	// 	}                                                        // INVERTED FULL PYRAMID 
+
+	// 	//stars
+	// 	for(int col=0; col<n-row; col=col+1) {
+	// 		cout << "* ";
+	// 	}
+	// 	cout << endl;
+	// }
 }
