25b3f712474872e8a6b6c8c7730006402a978eaa
Aniket Sunil Ghodake <74956511+AniGhodake@users.noreply.github.com>
2023-11-12 21:58:26 +0530

    Update fancyDiamond.cpp

--- fancyDiamond.cpp
+++ fancyDiamond.cpp
@@ -32,10 +32,10 @@
 		}
 		cout << endl;
 	}
- // for(int row=0;row<n; row=row+1) {
-	// 	//inverted pyramid 1
-	// 	for(int col=0; col<row+1; col=col+1) {
-	// 		cout << "* ";
+ for(int row=0;row<n; row=row+1) {
+		//inverted pyramid 1
+		for(int col=0; col<row+1; col=col+1) {
+			cout << "* ";
 		}
 		//full pyramid 1
 		for(int col=0;col<2*n-2*row-1; col=col+1) {
