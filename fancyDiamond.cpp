e36667f8ac8617c5e2eecded632338f905489b00
Aniket Sunil Ghodake <74956511+AniGhodake@users.noreply.github.com>
2023-11-28 20:54:21 +0530

    Update fancyDiamond.cpp

--- fancyDiamond.cpp
+++ fancyDiamond.cpp
@@ -32,19 +32,4 @@
 		}
 		cout << endl;
 	}
- for(int row=0;row<n; row=row+1) {
-		//inverted pyramid 1
-		for(int col=0; col<row+1; col=col+1) {
-			cout << "* ";
-		}
-		//full pyramid 1
-		for(int col=0;col<2*n-2*row-1; col=col+1) {
-			cout << "  ";                                       // fancy pattern part 2
-		}                                                      // diamond of spaces surrounded by stars
 
-		//inverted pyramid 2
-		for(int col=0; col<row+1; col=col+1) {
-			cout << "* ";
-		}
-		cout << endl;
-	}
